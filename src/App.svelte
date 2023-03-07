7ea4a58fa63ccf7a952e99b1f28b2e1fad943bc7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-07 15:30:22 -0500

    Add: Form markup

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,41 @@
   import './index.css';
 </script>
 
-<main>
-  <h1 class="bg-red-200">Hello</h1>
+<main class="min-h-screen">
+  <section class="flex flex-col gap-4 max-w-screen-2xl mx-auto py-2">
+    <form class="flex flex-col gap-4 mx-2">
+      <div class="flex flex-col gap-2">
+        <label for="fullname">Nombre Completo</label>
+        <input
+          class="border p-2"
+          id="fullname"
+          type="text"
+          placeholder="John Doe"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label for="email">Correo Electrónico</label>
+        <input
+          class="border p-2"
+          id="email"
+          type="text"
+          placeholder="johndoe@mail.com"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <label for="content">Describe tu Idea</label>
+        <textarea
+          class="border p-2 resize-none"
+          id="content"
+          placeholder="johndoe@mail.com"
+        />
+      </div>
+      <div class="flex flex-col gap-2">
+        <button
+          class="bg-indigo-400 p-2 text-white text-lg font-semibold hover:bg-indigo-500 rounded-md"
+          >Enviar</button
+        >
+      </div>
+    </form>
+  </section>
 </main>
