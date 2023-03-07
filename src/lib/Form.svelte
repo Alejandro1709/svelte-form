632510630b80f62b27c5666f1097820072432802
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-07 15:32:09 -0500

    Extract: Form to its own Component

---
+++ src/lib/Form.svelte
@@ -0,0 +1,34 @@
+<form class="flex flex-col gap-4 mx-2">
+  <div class="flex flex-col gap-2">
+    <label for="fullname">Nombre Completo</label>
+    <input
+      class="border p-2"
+      id="fullname"
+      type="text"
+      placeholder="John Doe"
+    />
+  </div>
+  <div class="flex flex-col gap-2">
+    <label for="email">Correo Electrónico</label>
+    <input
+      class="border p-2"
+      id="email"
+      type="text"
+      placeholder="johndoe@mail.com"
+    />
+  </div>
+  <div class="flex flex-col gap-2">
+    <label for="content">Describe tu Idea</label>
+    <textarea
+      class="border p-2 resize-none"
+      id="content"
+      placeholder="johndoe@mail.com"
+    />
+  </div>
+  <div class="flex flex-col gap-2">
+    <button
+      class="bg-indigo-400 p-2 text-white text-lg font-semibold hover:bg-indigo-500 rounded-md"
+      >Enviar</button
+    >
+  </div>
+</form>
